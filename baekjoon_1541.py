88ec59aafa67433f8ded835a4297c588936d8327
JW YUN <savior371@gmail.com>
2022-01-06 10:36:36 +0900

    baekjoon_1541
    그리디 문제풀이 완료

--- baekjoon_1541.py
+++ baekjoon_1541.py
@@ -1,36 +1,12 @@
-exp = input()
-
-temp = 0
-isFirst = True
-new_exp = ""
-b = ""
-for i in range(len(exp)):
-    if exp[i] == '+' or exp[i] == '-':
-        num = int(exp[temp:i])
-        new_exp += str(num)
-        new_exp += exp[i]
-        temp = i + 1
-    
-    if i == len(exp) - 1:
-        num = int(exp[temp:])
-        new_exp += str(num)
-        
-for i in new_exp:
-    if i == '-':
-        if isFirst:
-            b += i + '('
-            isFirst = False
-        else:
-            b += ')' + i + '('
-            isFirst = True
-    else:
-        b += i
-
-
-if isFirst:
-    b += ')'
-        
-print(b)
-result = eval(b)
-
-print(result)    
+a = input().split('-')
+num = []
+for i in a:
+    cnt = 0
+    s = i.split('+')
+    for j in s:
+        cnt += int(j)
+    num.append(cnt)
+n = num[0]
+for i in range(1, len(num)):
+    n -= num[i]
+print(n)
