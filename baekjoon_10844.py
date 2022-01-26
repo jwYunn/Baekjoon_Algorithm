9594a0228524017d3ae882d0c9805bd5211f2162
JW YUN <savior371@gmail.com>
2022-01-26 16:01:00 +0900

    baekjoon
    dp 문제 풀이완료

--- baekjoon_10844.py
+++ baekjoon_10844.py
@@ -1,10 +1,13 @@
-N = int(input())
-
-s = [0] * (N + 1)
-s[1] = 9
-
-for i in range(2, N + 1):
-    one_count = 2 ** (i - 2)
-    s[i] = (s[i - 1] - one_count) * 2 + one_count * 1
-    
-print(s[N])
+n = int(input())
+dp = [[0 for i in range(10)] for j in range(101)]
+for i in range(1, 10):
+    dp[1][i] = 1
+for i in range(2, n + 1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i - 1][1]
+        elif j == 9:
+            dp[i][j] = dp[i - 1][8]
+        else:
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
+print(sum(dp[n]) % 1000000000)
