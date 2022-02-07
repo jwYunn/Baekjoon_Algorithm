7990305f0173cf39a7c599bad0e63a1f94259061
JW YUN <savior371@gmail.com>
2022-02-07 15:11:26 +0900

    baekjoon_2263
    트리 선위순회찾기 풀이 완료

--- baekjoon_2263.py
+++ baekjoon_2263.py
@@ -1,31 +1,47 @@
-n = int(input())
-
-tree = {key:[0,0] for key in range(1, n + 1)}
-inorder = list(map(int, input().split()))
-postorder = list(map(int, input().split()))
+import sys
+sys.setrecursionlimit(10**5)
 
 def solve(in_start, in_end, post_start, post_end):
+    global pre_order
+
+    if in_end - in_start < 0:
+        return
     
-    root = postorder[post_end]
+    if in_end - in_start == 0:
+        pre_order.append(in_order[in_start])
+        return
+    
+    # find in_order root index
+    root = post_order[post_end]
+    
+    pre_order.append(root)
         
-    index = inorder.index(root)
-    postorder.index(root)
+    in_order_root_index = -1
+    for i in range(in_start, in_end + 1):
+        if root == in_order[i]:
+            in_order_root_index = i
+            break
     
-    tree[root][0] = 
+    in_left_start = in_start
+    in_left_end = in_order_root_index - 1 
+
+    in_right_start = in_order_root_index + 1
+    in_right_end = in_end
+
+    post_left_start = post_start      
+    post_left_end = post_start + (in_left_end - in_left_start)
+
+    post_right_start = post_left_end + 1
+    post_right_end = post_right_start + (in_right_end - in_right_start)
     
-    solve(in_start, index - 1, post_start, post_start + (index - in_start - 1))
-    solve(index + 1, in_end, )
+    solve(in_left_start, in_left_end, post_left_start, post_left_end)
+    solve(in_right_start, in_right_end, post_right_start, post_right_end)
+
     
-    return
-
-def preorder(node):
-    
-    
-    return
-
-
-
-
-
-solve(0, len(inorder) - 1, 0, len(postorder))
-preorder(postorder[-1])
+if __name__ == "__main__":
+    n = int(input())
+    in_order = list(map(int, input().split()))
+    post_order = list(map(int, input().split()))
+    pre_order = []
+    solve(0, len(in_order) - 1, 0, len(post_order) - 1)
+    print(*pre_order)
